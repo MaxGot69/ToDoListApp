718501eb51e0bbb55da247cc5a5b1fce324c81c8
Maxim Gotovchenko <gotovchenko04@bk.ru>
2025-06-26 18:32:18 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Maxim Gotovchenko on 26.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
